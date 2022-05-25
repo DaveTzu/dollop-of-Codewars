446430ff3fe46a09374d5474699841032cfe8c96
Dave <DisembodiedMinstrel@gmail.com>
2022-05-25 12:51:21 -0600

    5.25.22

---
+++ 8kyu/5.25.22.js
@@ -0,0 +1,12 @@
+function expressionMatter(a, b, c) {
+    let arr = []
+    arr.push(a*b*c)
+    arr.push(a+b*c)
+    arr.push(a*b+c)
+    arr.push((a+b)*c)
+    arr.push(a*(b+c))
+    arr.push((a*b)+c)
+    arr.push(a+(b*c))
+    arr.push(a+b+c)  
+    return Math.max(...arr)
+}
