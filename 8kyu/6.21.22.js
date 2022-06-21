e3ca3c5ca6ae97b63b3e5da027589d8be3ef498c
Dave <DisembodiedMinstrel@gmail.com>
2022-06-21 16:36:12 -0600

    6.21.22

---
+++ 8kyu/6.21.22.js
@@ -0,0 +1,9 @@
+var humanYearsCatYearsDogYears = function(humanYears) {
+    if (humanYears === 1){
+      return [humanYears, 15, 15]
+    } else if (humanYears === 2){
+      return [humanYears, 24, 24]
+    } else {
+      return [humanYears, 24 + (4 * (humanYears-2)),24 + (5 * (humanYears-2)) ]
+    }
+  }
